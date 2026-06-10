ef2e44ee351724f773ed87f6fa6fa3c7f386b08c
krishnalath <krishnalath.cs24@bmsce.ac.in>
2026-06-10 21:27:29 +0530

    Refactor FCFS scheduling algorithm and add Gantt chart

--- fcfs.c
+++ fcfs.c
@@ -1,75 +1,75 @@
-#define max 10
+// Online C compiler to run C program online
 #include <stdio.h>
-
-struct process {
-    int at, bt;
-    int ct, wt, tat;
+struct process{
+  int id,at,bt,ct,wt,tat;  
 };
 
-void FCFS(struct process p[], int n)
-{
-    int t = 0;
-    float total_wt = 0, total_tat = 0;
-
-    for(int i = 0; i < n - 1; i++)
-    {
-        for(int j = i + 1; j < n; j++)
-        {
-            if(p[i].at > p[j].at)
-            {
-                struct process temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
+void sort(struct process p[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(p[j].at>p[j+1].at){
+                struct process temp = p[j];
+                p[j]=p[j+1];
+                p[j+1]=temp;
             }
         }
     }
-
-    for(int i = 0; i < n; i++)
-    {
-        if(t < p[i].at)
-            t = p[i].at;
-
-        p[i].ct = t + p[i].bt;
-        p[i].tat = p[i].ct - p[i].at;
-        p[i].wt = p[i].tat - p[i].bt;
-
-        total_wt += p[i].wt;
-        total_tat += p[i].tat;
-
-        t = p[i].ct;
-    }
-
-    printf("\nAT\tBT\tCT\tWT\tTAT\n");
-
-    for(int i = 0; i < n; i++)
-    {
-        printf("%d\t%d\t%d\t%d\t%d\n",
-               p[i].at, p[i].bt,
-               p[i].ct, p[i].wt, p[i].tat);
-    }
-
-    printf("\nAverage WT = %.2f", total_wt/n);
-    printf("\nAverage TAT = %.2f\n", total_tat/n);
 }
 
-int main()
-{
-    int n;
-    struct process p[max];
+void fcfs(struct process p[],int n){
+    int t=0;
+    float tWT=0,tTAT=0;
+    for(int i=0;i<n;i++){
+        if(p[i].at>t)
+            t=p[i].at;
+        p[i].ct = t+p[i].bt;
+        p[i].tat = p[i].ct-p[i].at;
+        p[i].wt = p[i].tat-p[i].bt;
+        t=p[i].ct;
+    }
+    printf("\n%-5s%-5s%-5s%-5s%-8s%-8s\n", "PID", "AT", "BT", "CT", "WT", "TAT");
+    for (int i=0; i<n; i++) {
+        printf("%-5d%-5d%-5d%-5d%-8.2f%-8.2f\n",
+               i, p[i].at, p[i].bt, p[i].ct,
+               (float)p[i].wt, (float)p[i].tat);
+        tWT+=p[i].wt;
+        tTAT+=p[i].tat;
+    }
+    tWT/=n;
+    tTAT/=n; printf("\nAverage Waiting Time = %.2f", tWT);
+    printf("\nAverage Turnaround Time = %.2f\n", tTAT);
+}
 
-    printf("Enter number of processes: ");
-    scanf("%d",&n);
+void ganttChart(struct process p[], int n)
+{
+    printf("\nGantt Chart:\n");
 
     for(int i=0;i<n;i++)
-    {
-        printf("AT of P%d: ",i);
-        scanf("%d",&p[i].at);
+        printf("| P%d ", i);
 
-        printf("BT of P%d: ",i);
-        scanf("%d",&p[i].bt);
+    printf("|\n");
+
+    printf("0");
+
+    for(int i=0;i<n;i++)
+        printf("    %d", p[i].ct);
+
+    printf("\n");
+}
+
+int main() {
+    int n;
+    struct process p[100];
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    for (int i=0; i<n; i++) {
+        printf("Enter arrival time for process %d: ", i);
+        scanf("%d", &p[i].at);
+        printf("Enter burst time for process %d: ", i);
+        scanf("%d", &p[i].bt);
     }
-
-    FCFS(p,n);
-
+    sort(p, n); 
+    fcfs(p, n);
+    ganttChart(p,n);
     return 0;
 }
