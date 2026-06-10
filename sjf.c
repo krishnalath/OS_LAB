4909ec858a20b96c2e6d2cbdbf81bb3874ddabb1
krishnalath <krishnalath.cs24@bmsce.ac.in>
2026-06-10 21:52:08 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,53 +1,57 @@
-#define max 10
 #include <stdio.h>
-
-struct process {
-    int at, bt;
-    int wt, tat, ct;
-    int done;
+struct process
+{
+    int at, bt, ct, wt, tat, done;
 };
 
-void SJF(struct process p[], int n)
+void sjf(struct process p[], int n)
 {
-    int completed = 0;
-    int t = 0;
+    int completed = 0, t = 0;
+    float awt = 0, atat = 0;
 
-    float total_wt = 0;
-    float total_tat = 0;
+    int gantt[100];
+    int times[101];
+    int k = 0;
 
-    while(completed < n)
+    times[0] = 0;
+
+    while (completed < n)
     {
         int idx = -1;
         int min_bt = 9999;
 
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            if(p[i].at <= t && p[i].done == 0)
+            if (p[i].at <= t && p[i].done == 0)
             {
-                if(p[i].bt < min_bt)
+                if (p[i].bt < min_bt)
                 {
                     min_bt = p[i].bt;
                     idx = i;
                 }
-                else if(p[i].bt == min_bt)
+                else if (p[i].bt == min_bt)
                 {
-                    if(p[i].at < p[idx].at)
+                    if (p[i].at < p[idx].at)
                         idx = i;
                 }
             }
         }
 
-        if(idx != -1)
+        if (idx != -1)
         {
             p[idx].ct = t + p[idx].bt;
             p[idx].tat = p[idx].ct - p[idx].at;
             p[idx].wt = p[idx].tat - p[idx].bt;
 
-            total_wt += p[idx].wt;
-            total_tat += p[idx].tat;
+            awt += p[idx].wt;
+            atat += p[idx].tat;
 
             p[idx].done = 1;
             completed++;
+
+            gantt[k] = idx;
+            times[k + 1] = p[idx].ct;
+            k++;
 
             t = p[idx].ct;
         }
@@ -58,38 +62,48 @@ void SJF(struct process p[], int n)
     }
 
     printf("\nID\tAT\tBT\tCT\tWT\tTAT\n");
-
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d\t%d\t%d\t%d\t%d\t%d\n",
-               i,p[i].at,p[i].bt,
-               p[i].ct,p[i].wt,p[i].tat);
+               i, p[i].at, p[i].bt,
+               p[i].ct, p[i].wt, p[i].tat);
     }
 
-    printf("\nAverage WT = %.2f",total_wt/n);
-    printf("\nAverage TAT = %.2f\n",total_tat/n);
+    printf("\nAverage Waiting Time = %.2f\n", awt / n);
+    printf("Average Turnaround Time = %.2f\n", atat / n);
+
+    // Gantt Chart
+    printf("\nGantt Chart:\n");
+
+    for (int i = 0; i < k; i++)
+        printf("| P%d ", gantt[i]);
+
+    printf("|\n");
+
+    printf("%d", times[0]);
+
+    for (int i = 1; i <= k; i++)
+        printf("    %d", times[i]);
+
+    printf("\n");
 }
 
 int main()
 {
     int n;
-    struct process p[max];
+    struct process p[100];
 
     printf("Enter number of processes: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("AT of P%d: ",i);
-        scanf("%d",&p[i].at);
-
-        printf("BT of P%d: ",i);
-        scanf("%d",&p[i].bt);
-
+        printf("Enter AT and BT for P%d: ", i);
+        scanf("%d%d", &p[i].at, &p[i].bt);
         p[i].done = 0;
     }
 
-    SJF(p,n);
+    sjf(p, n);
 
     return 0;
 }
